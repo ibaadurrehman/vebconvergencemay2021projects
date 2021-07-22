2881483fd1264b264680336ff311086c4df8f076
eswaribala <parameswaribala@gmail.com>
2021-07-22 19:23:10 +0530

    updated

--- inventoryservicedocker/src/main/java/com/veb/inventoryservice/InventoryserviceApplication.java
+++ inventoryservicedocker/src/main/java/com/veb/inventoryservice/InventoryserviceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 //@EnableEurekaClient
